567062d48e87b9085f6ef8b731b8728e987bbaa2
aitap <krylov.r00t@gmail.com>
2019-05-07 23:27:22 +0300

    minimal script that manages to decode files I have

---
+++ decode.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use autodie;
+use Fcntl 'SEEK_SET';
+
+for my $src (@ARGV) {
+	open my $rfh, "<:raw", $src;
+
+	(my $dest = $src) =~ s/\.spd$/.txt/;
+	open my $wfh, ">", $dest;
+
+	# 1024 looks like a fine header size, but there is
+	# additional shift by 5 bytes for some reason
+	seek $rfh, 1029, SEEK_SET;
+
+	# the spectrum is stored in pairs of lambda, 1-T
+	local $/ = \16;
+	print $wfh (join("\t", unpack "d<2", $_), "\n")
+		while (<$rfh>);
+}
